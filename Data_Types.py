ec47efa5412afbd0baefcbe521c6a1df01728095
Ibtihaj Ahmad <77004751+ibtihajahmadkhan@users.noreply.github.com>
2022-02-07 19:22:54 -0800

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,38 @@
+## DataTypes
+
+x = "Hello World" 			    # str 	
+x = 20 					    # int 	
+x = 20.5 				    # float 	
+x = 1j 					    # complex 	
+x = ["apple", "banana", "cherry"] 	    # list 	
+x = ("apple", "banana", "cherry") 	    # tuple 	
+x = range(6) 				    # range 	
+x = {"name" : "John", "age" : 36} 	    # dict 	
+x = {"apple", "banana", "cherry"} 	    # set 	
+x = frozenset({"apple", "banana", "cherry"})# frozenset 	
+x = True 				    # bool
+					
+
+# Data Type Conversion
+x = str("Hello World")                      # str 	
+x = int(20)                                 # int 	
+x = float(20.5)                             # float 	
+x = complex(1j)                             # complex 	
+x = list(("apple", "banana", "cherry"))     # list 	
+x = tuple(("apple", "banana", "cherry"))    # tuple 	
+x = range(6)                                # range 	
+x = dict(name="John", age=36)               # dict 	
+x = set(("apple", "banana", "cherry")) 	    # set 	
+x = frozenset(("apple", "banana", "cherry"))# frozenset 	
+x = bool(5)                                 # bool
+
+# Strings in Python
+str = 'Hello World!'
+print (str)             # Prints complete string
+print (str[0] )         # Prints first character of the string
+print (str[2:5] )       # Prints characters starting from 3rd to 5th
+print (str[2:] )        # Prints string starting from 3rd character
+print (str * 2 )        # Prints string two times
+print (str + 'TEST' )   # Prints concatenated string
+
+
