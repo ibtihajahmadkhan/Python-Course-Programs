fbd24ea4d247dc319cc4a60d0a1be90fae4fe102
Ibtihaj Ahmad <77004751+ibtihajahmadkhan@users.noreply.github.com>
2022-02-07 20:03:18 -0800

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,45 @@
+## Loops
+
+# While Loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+## Break Statement
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1 
+
+
+# For Loop
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in "banana":
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+# range
+for x in range(6):
+  print(x)
+for x in range(2, 6):
+  print(x)
+for x in range(2, 14, 3):
+  print(x)
+
+
+
+
+
+
+
+
