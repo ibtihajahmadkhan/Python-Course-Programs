56973cce9d50fb1a8e54bd42c03a0c2a84338b83
Ibtihaj Ahmad <77004751+ibtihajahmadkhan@users.noreply.github.com>
2022-02-07 19:25:57 -0800

    Add files via upload

---
+++ Hello_Word.py
@@ -0,0 +1,13 @@
+print("Hello World. This is my First Program. ") 
+print("I am Ibtihaj Ahmad (Lele). ") 
+print("I am printing a number ", 2)
+
+# Store a number
+mynumb = 2
+# Print a stored Number
+print("I am printing my number ", mynumb)
+
+# Store alphabets or words
+myname = 'Ibtihaj'
+# Print a stored Number
+print("My name is  ", myname)
